ab834a41e8a02e52331f4b3d1eb238344772aabb
amoeba224 <80118706+amoeba224@users.noreply.github.com>
2022-07-24 16:45:50 +0900

    Update 1upperlower.c

--- 2주차/1upperlower.c
+++ 2주차/1upperlower.c
@@ -7,7 +7,7 @@ int main(void) {
 
   fgets(input, sizeof(input), stdin);
 
-  for (int i = 0; i <= strlen(input); i++) {https://github.com/amoeba224/c_study/blob/main/2%EC%A3%BC%EC%B0%A8/upperlower.c
+  for (int i = 0; i <= strlen(input); i++) {
     if (input[i] >= 65 && input[i] <= 90) {
       input[i] = input[i] + 32; //대문자
     } else if (input[i] >= 97 && input[i] <= 122) {
