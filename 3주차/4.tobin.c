b85fcfe14d34ca9a34a640111672db739177df2a
amoeba224 <80118706+amoeba224@users.noreply.github.com>
2022-07-31 17:44:21 +0900

    Update 4.tobin.c

--- 3주차/4.tobin.c
+++ 3주차/4.tobin.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
-
-int n, k;
-int num[30];
+int n;
+int num[100];
+int cnt = 0;
 
 void getResult(int x, int y) {
-  if(x>=n){
-    if(y == k) {
-      for(int i=0; i<n; i++){
-        printf("%d", num[i]);
-      }printf("\n");
+	if (x == n) {
+		cnt++;
+		printf("%d", num[0]);
+		for (int i = 1; i < y; i++){
+			printf("+%d", num[i]);
     }
-    else
-      return;
-  } else { // x<n
-    if (y>=k){
-      for(int i=0; i<n; i++){
-        printf("%d", num[i]);
-      }printf("\n");
-    }
-    else { // y<n
-      num[x] = 1;
-      getResult(x+1, y+1);
-      num[x] = 0;
-      getResult(x+1, y);
-    }
-  }
+		printf("\n");
+	}
+	if (x > n)
+		return;
+
+	for (int i = n - 1; i >= 1; i--) {
+		if (y == 0) {
+			num[y] = i;
+			getResult(x + i, y + 1);
+			num[y] = 0;
+		}
+		else {
+			if (num[y - 1] >= i) {
+				num[y] = i;
+				getResult(x + i, y + 1);
+				num[y] = 0;
+			}
+		}
+	}
 }
 
 int main() {
-  scanf("%d %d", &n, &k);
-
-  getResult(0, 0);
-
-  return 0;
+	scanf("%d", &n);
+	getResult(0, 0);
+	printf("%d", cnt);
+	return 0;
 }
