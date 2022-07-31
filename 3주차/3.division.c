871d31bf299e9dc841d2c46cbfe49c79978e5754
amoeba224 <80118706+amoeba224@users.noreply.github.com>
2022-07-31 17:47:15 +0900

    Update 3.division.c

--- 3주차/3.division.c
+++ 3주차/3.division.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
-
 int n;
-int num = 0;
+int num[100];
 int idx = 0;
 
-void getResult(int x) {
-  int j = x - 1;
-  int k = 0;
-  if (j > 0) {
-    for (int i = 1; i <= x / 2; i++) {
-      j = x - i;
-      k = i;
-      if (num == 0) {
-        if (j >= k) {
-          printf("%d+%d\n", j, k);
-          idx++;
-        } else {
-          break;
-        }
-      } else {
-        printf("%d+%d", j, k);
-        idx++;
-        for (int m = 1; m <= num; m++) {
-          printf("+1");
-        }
-        printf("\n");
-      }
+void getResult(int x, int y) {
+	if (x == n) {
+		idx++;
+		printf("%d", num[0]);
+		for (int i = 1; i < y; i++){
+			printf("+%d", num[i]);
     }
-    num++;
-    getResult(x - 1);
-  }
+		printf("\n");
+	}
+	if (x > n)
+		return;
+
+	for (int i = n - 1; i >= 1; i--) {
+		if (y == 0) {
+			num[y] = i;
+			getResult(x + i, y + 1);
+			num[y] = 0;
+		}
+		else {
+			if (num[y - 1] >= i) {
+				num[y] = i;
+				getResult(x + i, y + 1);
+				num[y] = 0;
+			}
+		}
+	}
 }
 
 int main() {
-  scanf("%d", &n);
-  getResult(n);
-  printf("%d", idx);
-  return 0;
+	scanf("%d", &n);
+	getResult(0, 0);
+	printf("%d", idx);
+	return 0;
 }
